bcefb42d5c61a174d63beebb4f13aa5550896bc6
nehara-peiris <shewmipeiris123@gmail.com>
2024-10-10 22:18:20 +0530

    update js

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
-
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () =>{
+function loadVoices() {
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-};
+    if (voices.length > 0) {
+        speech.voice = voices[0];
+        voiceSelect.innerHTML = '';
+        voices.forEach((voice, i) => {
+            let option = new Option(voice.name + ' (' + voice.lang + ')', i);
+            voiceSelect.options.add(option);
+        });
+    }
+}
 
-voiceSelect.addEventListener("change" , () =>{
-    speech.voice = voices[voiceSelect.value]
+// Call `loadVoices()` both on voice change and after a short delay
+window.speechSynthesis.onvoiceschanged = loadVoices;
+
+// For mobile, try calling manually after a small delay
+setTimeout(() => {
+    if (voices.length === 0) {
+        loadVoices(); // Try loading again in case voices are delayed
+    }
+}, 1000);
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
 });
 
-document.querySelector("button").addEventListener("click", () =>{
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
